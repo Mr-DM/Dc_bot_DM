f39551801accd0aa47ead584a099407dc07a624d
Mr-DM <161638656+Mr-DM@users.noreply.github.com>
2024-04-27 13:55:24 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+import discord
+from function import gen_pass , coin_toss 
+
+
+
+# Переменная intents - хранит привилегии бота
+intents = discord.Intents.default()
+# Включаем привелегию на чтение сообщений
+intents.message_content = True
+# Создаем бота в переменной client и передаем все привелегии
+client = discord.Client(intents=intents)
+
+
+
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hi!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\\U0001f642")
+    elif message.content.startswith('$new pass'):
+        await message.channel.send(gen_pass(8))
+    elif message.content.startswith('$coin toss'):
+        await message.channel.send(coin_toss)
+    else:
+        await message.channel.send(message.content)
+
+client.run("Токен")
