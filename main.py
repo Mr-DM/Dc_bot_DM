9a9684f8fc662920eb8b44f6462399f75f492e58
Mr-DM <161638656+Mr-DM@users.noreply.github.com>
2024-05-09 19:20:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import discord
 from discord.ext import commands
 from function import gen_pass , coin_toss , gen_emo 
+import os
+import random
+import requests
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -45,5 +48,56 @@ async def my_code(ctx):
 @bot.command()
 async def helps(ctx):  
     await ctx.send(f'В боте {bot.user} есть функцы как (hello, coin_flip, new_pass, emoji, my_code, repeat, helps) не забудь в начале $ ')
+
+
+@bot.command()
+async def mem(ctx):
+    # А вот так можно подставить имя файла из переменной!
+    img_name = random.choice(os.listdir('images'))
+    with open(f'images/{img_name}', 'rb') as f:   
+            picture = discord.File(f)
+   # Можем передавать файл как параметр!
+    await ctx.send(file=picture)
+
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('duck')
+async def duck(ctx):
+    '''По команде duck вызывает функцию get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+
+
+
+def get_dog_image_url():    
+    url = 'https://random.dog/woof.json'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('dog')
+async def dog(ctx):
+    image_url = get_dog_image_url()
+    await ctx.send(image_url)
+
+def get_fox_image_url():    
+    url = 'https://randomfox.ca/floof/'
+    res = requests.get(url)
+    data = res.json()
+    return data['image']
+
+
+@bot.command('fox')
+async def fox(ctx):
+    image_url = get_fox_image_url()
+    await ctx.send(image_url)
 
 bot.run("Токен")
